8827c307943d83f5a3c46ccfab41f6848e7a4fb8
Mohand hatem <146125801+Mohand-Hatem@users.noreply.github.com>
2026-03-29 17:49:53 +0200

    modify

--- controllers/tickets.controller.js
+++ controllers/tickets.controller.js
@@ -1,32 +1,33 @@
-import asyncHandler from "express-async-handler"
-import ticketModel from "../models/ticket.model.js"
+import asyncHandler from "express-async-handler";
+import ticketModel from "../models/ticket.model.js";
 
+export const createTicket = asyncHandler(async (req, res) => {
+  const { userId, agentId, title } = req.body;
 
-export const createTicket = asyncHandler(async (req ,res) => {
-    const {userId , agentId , title} = req.body
+  const creTicket = await ticketModel.create({ userId, agentId, title });
+  const newTicket = await ticketModel
+    .findById(creTicket._id)
+    .populate("userId", "username role")
+    .populate("agentId", "username role");
+  res.status(201).json({ message: "success", newTicket });
+});
 
-    const creTicket = await ticketModel.create({userId,agentId,title})
-    const newTicket = await ticketModel.findById(creTicket._id).populate("userId","username role").populate("agentId","username role")
-    res.status(201).json({message:"success",newTicket})
-})
+export const getTicketById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
+  const foundedTicket = await ticketModel
+    .findById(id)
+    .populate("userId", "username role")
+    .populate("agentId", "username role");
 
+  res.status(200).json({ message: "success", foundedTicket });
+});
 
-export const getTicketById = asyncHandler(async (req,res)=>{
-    const {id}= req.params
+export const getAllTickets = asyncHandler(async (req, res) => {
+  const AllTickets = await ticketModel
+    .find()
+    .populate("userId", "username role -_id")
+    .populate("agentId", "username role -_id");
 
-    const foundedTicket = await ticketModel.findById(id).populate("userId","username role").populate("agentId","username role")
-
-    res.status(200).json({message:"success",foundedTicket})
-
-})
-
-
-
-export const getAllTickets = asyncHandler(async (req,res)=>{
-   
-    const AllTickets = await ticketModel.find().populate("userId","username role -_id").populate("agentId","username role -_id")
-
-    res.status(200).json({message:"success",AllTickets})
-
-})
+  res.status(200).json({ message: "success", AllTickets });
+});
